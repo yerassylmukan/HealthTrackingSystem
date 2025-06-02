737708401fdf6fb987754828dc5cfe689b373432
yerassylmukan <yerassyl.mukan@outlook.com>
2025-06-02 23:18:30 +0500

    Add ui and nginx conf

--- src/WebApplication1/Models/HealthEntryResponseModel.cs
+++ src/WebApplication1/Models/HealthEntryResponseModel.cs
@@ -3,8 +3,12 @@
 public class HealthEntryResponseModel
 {
     public DateTime Date { get; set; }
+    
     public int Calories { get; set; }
+    
     public int Steps { get; set; }
+    
     public float SleepHours { get; set; }
+    
     public string Mood { get; set; }
 }
